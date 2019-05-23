47cc92b14a9562a3f0ef766c02248f7540386562
Balazs Scheidler <balazs.scheidler@oneidentity.com>
2019-05-23 12:17:35 +0200

    dbld: add the ability to pass environment variables and command line arguments to build commands
    Signed-off-by: Balazs Scheidler <balazs.scheidler@oneidentity.com>

--- dbld/functions.sh
+++ dbld/functions.sh
@@ -5,4 +5,17 @@ function get_version() {
 	scripts/version.sh
 }
 
+function run_build_command() {
+	OS=$(echo $OS_PLATFORM |  cut -d '-' -f 1)
+
+	# we sort longer strings in front, to make more specific items
+	# first.
+
+	egrep -e "^${OS}([^-]|$)" -e "^${OS_PLATFORM}" /source/dbld/build.manifest | sort -r | head -1 | while read os env cmdline; do
+	        env=$(echo ${env} | tr ',' ' ')
+		echo "Running build as: " env $env "$@" $cmdline
+	        env $env "$@" $cmdline
+	done
+}
+
 VERSION=$(get_version)
